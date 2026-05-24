fbe00e5b64caabb0f00e4bb156ef1f1450113b8b
mcpicornell <marc.castro.picornell@gmail.com>
2026-05-24 12:08:34 +0200

    feat: logout view

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -4,8 +4,10 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 # Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
 from .forms import LoginForm, SignUpForm
 
 
@@ -15,7 +17,6 @@ def login_view(request):
     msg = None
 
     if request.method == "POST":
-
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -24,9 +25,9 @@ def login_view(request):
                 login(request, user)
                 return redirect("/")
             else:
-                msg = 'Invalid credentials'
+                msg = "Invalid credentials"
         else:
-            msg = 'Error validating the form'
+            msg = "Error validating the form"
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
@@ -49,8 +50,18 @@ def register_user(request):
             # return redirect("/login/")
 
         else:
-            msg = 'Form is not valid'
+            msg = "Form is not valid"
     else:
         form = SignUpForm()
 
-    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+    return render(
+        request,
+        "accounts/register.html",
+        {"form": form, "msg": msg, "success": success},
+    )
+
+
+@login_required(login_url="/login/")
+def logout_view(request):
+    logout(request)
+    return redirect("login")
