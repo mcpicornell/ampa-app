c50995528d2b805b59d202f2018b3a318b9d9b29
mcpicornell <marc.castro.picornell@gmail.com>
2026-06-13 13:48:10 +0200

    feat: storage without django cache

--- apps/home/ampa/services/ampa_files_storage.py
+++ apps/home/ampa/services/ampa_files_storage.py
@@ -1,9 +1,9 @@
+import threading
 import uuid
 from functools import lru_cache
 from typing import BinaryIO, Protocol
 
-from django.conf import settings
-from django.core.cache import cache
+from cachetools import TTLCache
 
 
 class AMPAImagesStorage(Protocol):
@@ -17,19 +17,28 @@ class AMPAImagesStorage(Protocol):
 class AMPAImagesCache(AMPAImagesStorage):
     PREFIX = "ampa-registry-"
 
+    def __init__(self, max_size: int, ttl_seconds: int):
+        self._internal_memory_cache = TTLCache(maxsize=max_size, ttl=ttl_seconds)
+        self._cache_lock = threading.Lock()
+
     def save_img(self, img_base64: str) -> str:
         registry_id = str(uuid.uuid4())
-        cache.set(
-            f"{self.PREFIX}{registry_id}",
-            img_base64,
-            timeout=settings.CACHE_EXPIRATION,
-        )
+        key = f"{self.PREFIX}{registry_id}"
+        with self._cache_lock:
+            self._internal_memory_cache[key] = img_base64
         return registry_id
 
     def get_img(self, registry_id: str) -> str | None:
-        return cache.get(f"{self.PREFIX}{registry_id}")
+        key = f"{self.PREFIX}{registry_id}"
+        with self._cache_lock:
+            return self._internal_memory_cache.get(key)
 
 
 @lru_cache
-def get_ampa_images_storage() -> AMPAImagesStorage:
-    return AMPAImagesCache()
+def get_ampa_images_storage(
+    max_size: int = 100, ttl_seconds: int = 1800
+) -> AMPAImagesStorage:
+    return AMPAImagesCache(
+        max_size=max_size,
+        ttl_seconds=ttl_seconds,
+    )
